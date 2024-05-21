eb8bed87abea6921f7aedef04ec180297b875720
GitJIHO <jiho99322@gmail.com>
2024-05-21 15:54:31 +0900

    first commit

---
+++ src/main/java/com/cums/cumshackathon/CumsHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.cums.cumshackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumsHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumsHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cums/cumshackathon/CumsHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cums.cumshackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumsHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
