6f291dc1450557fc0a143e040874e78dbfe3908e
GitJIHO <jiho99322@gmail.com>
2024-05-28 23:06:51 +0900

    feat: 내 정보 카드 형식으로 나오는 기능 추가, 로그인 하지 않을 시 접근 거부기능 추가, 게시판 버튼 색상 통일

--- src/main/java/com/cums/MainController.java
+++ src/main/java/com/cums/MainController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    private final String FIVE_URL = "https://859e-115-88-57-101.ngrok-free.app";
-    private final String EIGHT_URL = "https://8a55-115-88-57-101.ngrok-free.app";
+    private final String FIVE_URL = "https://180f-115-88-57-101.ngrok-free.app";
+    private final String EIGHT_URL = "https://75b7-115-88-57-101.ngrok-free.app";
 
 //    private final String FIVE_URL = "http://localhost:5000";
 //    private final String EIGHT_URL = " http://localhost:8080";
