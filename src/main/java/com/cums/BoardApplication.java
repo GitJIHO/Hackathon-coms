516b3aa7deabf2ed8aadc58db79402acca27f063
geon <kky8586696@naver.com>
2024-05-28 23:30:01 +0900

    feat: 로그인,회원가입,내정보창에서 네비바 글꼴적용안되는거 수정

--- src/main/java/com/cums/BoardApplication.java
+++ src/main/java/com/cums/BoardApplication.java
@@ -6,25 +6,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication implements CommandLineRunner  {
-
+public class BoardApplication   {
+//implements CommandLineRunner
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-//		String pythonExecutable = "myenv/bin/python3"; // macOS/Linux
-		 String pythonExecutable = "myenv\\Scripts\\python"; // Windows
-
-		String scriptPath = "facedetector.py";
-
-		try {
-			ProcessBuilder processBuilder = new ProcessBuilder(pythonExecutable, scriptPath);
-			processBuilder.inheritIO();
-			Process process = processBuilder.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+////		String pythonExecutable = "myenv/bin/python3"; // macOS/Linux
+//		 String pythonExecutable = "myenv\\Scripts\\python"; // Windows
+//
+//		String scriptPath = "facedetector.py";
+//
+//		try {
+//			ProcessBuilder processBuilder = new ProcessBuilder(pythonExecutable, scriptPath);
+//			processBuilder.inheritIO();
+//			Process process = processBuilder.start();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
